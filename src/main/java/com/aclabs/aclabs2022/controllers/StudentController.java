25d699f8b72f14925c4cd77f849556caf3007283
cristian.dina <cristi.dina93@yahoo.com>
2022-05-13 21:49:57 +0300

    Lab 6 source code - docker stuff

--- src/main/java/com/aclabs/aclabs2022/controllers/StudentController.java
+++ src/main/java/com/aclabs/aclabs2022/controllers/StudentController.java
@@ -3,6 +3,8 @@ package com.aclabs.aclabs2022.controllers;
 import com.aclabs.aclabs2022.model.Student;
 import com.aclabs.aclabs2022.services.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,22 +16,27 @@ public class StudentController {
     StudentService homeService;
 
     @GetMapping("/getAllStudents")
-    public List<Student> getAllStudents(){
-        return homeService.getAllStudents();
+    public ResponseEntity<List<Student>> getAllStudents() {
+        return new ResponseEntity<>(homeService.getAllStudents(), HttpStatus.OK);
     }
 
     @PostMapping("/addStudent")
-    public void addStudent(@RequestBody Student student){
+    public ResponseEntity<Object> addStudent(@RequestBody Student student) {
         homeService.addStudent(student);
+        return new ResponseEntity<>("Student with id: " + student.getId() + " was created.", HttpStatus.OK);
     }
 
     @PutMapping("/updateStudent")
-    public void updateStudent(@RequestBody Student updatedStudent){
+    public ResponseEntity<Object> updateStudent(@RequestBody Student updatedStudent) {
+        homeService.studentExists(updatedStudent.getId());
         homeService.updateStudent(updatedStudent);
+        return new ResponseEntity<>("Student with id: " + updatedStudent.getId() + " was updated.", HttpStatus.OK);
     }
 
     @DeleteMapping("/deleteStudent/{id}")
-    public void deleteStudent(@PathVariable String id){
+    public ResponseEntity<Object> deleteStudent(@PathVariable String id) {
+        homeService.studentExists(id);
         homeService.deleteStudent(id);
+        return new ResponseEntity<>("Student with id: " + id + " was deleted.", HttpStatus.OK);
     }
 }
